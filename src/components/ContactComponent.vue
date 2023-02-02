e90a405a5369221d9725e8fd5de8278ba4b038a3
Mampuya wa Lungila <mampuyawalungila@gmail.com>
2023-02-02 14:55:11 +0100

    commit ajout routes et differentes pages

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        contact
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactComponent',
+  
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from "vue-router";
-//import HomeComponent from './components/HomeComponent.vue'
+import HomeComponent from './components/HomeComponent.vue'
+import ProductComponent from './components/ProductComponent.vue';
+import AboutComponent from './components/AboutComponent.vue'
+import CartComponent from './components/CartComponent.vue'
+import ContactComponent from './components/ContactComponent.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 
+
 const routes = [
-  //{ path: "/", name: 'home', component: HomeComponent },
-  
+  { path: "/", name: 'home', component: HomeComponent },
+  { path: "/product", name: 'product', component: ProductComponent },
+  { path: "/About", name: 'about', component: AboutComponent },
+  { path: "/Contact", name: 'contact', component: ContactComponent },
+  { path: "/Cart", name: 'Cart', component: CartComponent }
 ];
 
 const router = new VueRouter({
